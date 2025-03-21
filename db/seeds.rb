874476dbca9b6612a432593d64ef84ff743803b1
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-21 20:46:51 +0530

    show messages on view with their user name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.create(body: "Hi I'am a new message", user_id: 1)
+Message.create(body: "hello I'am a new message", user_id: 2)
+Message.create(body: "Hi bhai log!", user_id: 2)
+Message.create(body: "hey I'am happy", user_id: 3)
+Message.create(body: "Hi lets meet yaro", user_id: 4)
+Message.create(body: "bro how are you", user_id: 5)
